154a45c38447846b9f9e58aa49c519fe3d296679
lychee <lychee@rb2.nl>
2024-07-18 14:34:38 +0800

    Init

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -13,21 +13,25 @@ declare namespace NodeJS {
 `
 await Bun.write('./global-env.d.ts', content)
 
-if (Bun.env.CLI_NAME) {
-  const BIN_PATH = `./bin/${Bun.env.CLI_NAME}`
-  await Bun.write(BIN_PATH, `#!/bin/sh
+if (Bun.env.ZSH) {
+  if (Bun.env.CLI_NAME) {
+    const BIN_PATH = `./bin/${Bun.env.CLI_NAME}`
+    await Bun.write(BIN_PATH, `#!/bin/sh
 exec bun "${path("./bin.ts")}" "$@"
 `)
-  const zshrc = await Bun.file(`${Bun.env.HOME}/.zshrc`).text()
-  const zshrcContent = zshrc.split('\n')
-  const alias = `alias ${Bun.env.CLI_NAME}="zsh ${path(BIN_PATH)}"`
-  if (zshrcContent.some(i => i === alias)) {
-    console.log(`zshrc already has ${alias}", skip add`)
+    const zshrc = await Bun.file(`${Bun.env.HOME}/.zshrc`).text()
+    const zshrcContent = zshrc.split('\n')
+    const alias = `alias ${Bun.env.CLI_NAME}="zsh ${path(BIN_PATH)}"`
+    if (zshrcContent.some(i => i === alias)) {
+      console.log(`zshrc already has ${alias}", skip add`)
+    } else {
+      await Bun.write(`${Bun.env.HOME}/.zshrc`, `${zshrc}\n${alias}`)
+      console.log(`zshrc add "${alias}", use "source ${Bun.env.HOME}/.zshrc"`)
+    }
   } else {
-    await Bun.write(`${Bun.env.HOME}/.zshrc`, `${zshrc}\n${alias}`)
-    console.log(`zshrc add "${alias}", use "source ${Bun.env.HOME}/.zshrc"`)
+    console.log("You can add CLI_NAME into your env and run `postinstall` again to use alias")
   }
 } else {
-  console.log("You can add CLI_NAME into your env and run `postinstall` again to use alias")
+  console.log("We only support ZSH now.")
 }
 
