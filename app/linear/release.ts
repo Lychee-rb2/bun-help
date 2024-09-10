1e216c8fc02357d0abad033c94a5645c000219c0
lychee <lychee@rb2.nl>
2024-09-10 14:06:36 +0800

    feat: add organization and repo validation and improve PR parsing in release notes

--- app/linear/release.ts
+++ app/linear/release.ts
@@ -5,14 +5,23 @@ import dayjs from 'dayjs'
 
 const validate = z.object({
   team: z.string(),
+  organization: z.string(),
+  repo: z.string(),
   target: z.string(),
   main: z.string()
 })
+const parsePr = (pr: string) => {
+  const matcher = pr.match(/\((#[0-9]*)\)$/)
+  if (!matcher) return (`- ${pr}`)
+  return `- [${pr}](https://github.com/${Bun.env.GIT_ORGANIZATION}/${Bun.env.GIT_REPO}/pull/${matcher[1]})`
+}
 export default async function () {
   const argv = parseArgv()
   const day = dayjs().format('YYYY_MM_DD')
   const { team, main, target } = validate.parse({
     team: Bun.env.LINEAR_TEAM,
+    organization: Bun.env.GIT_ORGANIZATION,
+    repo: Bun.env.GIT_REPO,
     target: argv.t || "release",
     main: argv.m || "main"
   })
@@ -21,12 +30,12 @@ export default async function () {
   ahead.forEach(title => {
     const index = behind.findIndex(v => title === v)
     if (index === -1) {
-      throw Error(`${title}在main中不存在`)
+      throw Error(`${title} is not in ${main}`)
     } else {
       behind.splice(index, 1)
     }
   })
-  const reg = /^(feat|fix)\(JOGG-(.*)\):/
+  const reg = /^(feat|fix)\(.*-(.*)\):/
   const pr2Issue: Record<string, number[]> = {}
   const issue2Pr: Record<string, string[]> = {}
   behind.forEach(title => {
@@ -66,29 +75,17 @@ export default async function () {
   const others: string[] = Object.keys(pr2Issue).filter(pr => !results.map(i => i.prs).flat().includes(pr))
 
   const note = results.sort((a, b) => a.issue.number - b.issue.number).reduce<string[]>((pre, cur) => {
-    pre.push(`### JOGG-${cur.issue.number}`)
+    pre.push(`### ${team}-${cur.issue.number}`)
     pre.push(`[${cur.issue.title}](${cur.issue.url})`)
     pre.push(' ')
     cur.prs.forEach((pr) => {
-      const matcher = pr.match(/\((#[0-9]*)\)$/)
-      if (matcher) {
-        pre.push(`- [${pr}](https://github.com/joggroup/jog-monorepo/pull/${matcher[1]})`)
-      } else {
-        pre.push(`- ${pr}`)
-      }
+      pre.push(parsePr(pr))
     })
     pre.push(' ')
     return pre
   }, [`# Release note: ${day}`, `## Diff ${target} and ${main}`])
   if (others.length) {
-    note.push(...[`### Others`, ...others.map(pr => {
-      const matcher = pr.match(/\((#[0-9]*)\)$/)
-      if (matcher) {
-        return (`- [${pr}](https://github.com/joggroup/jog-monorepo/pull/${matcher[1]})`)
-      } else {
-        return (`- ${pr}`)
-      }
-    }), ''])
+    note.push(...[`### Others`, ...others.map(pr => parsePr(pr)), ''])
   }
   const path = await tempFile(`${target}-${main}-${day}.md`, note.join('\n'))
   cli(`open ${path}`)
