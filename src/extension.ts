05543a969b145845f520d93f442cfbb7d54502d9
lychee <linzhihui2022@outlook.com>
2025-03-23 22:46:37 +0800

    refactor: comment out Vercel integration in extension.ts
    - Commented out the VercelTreeDataProvider initialization and related configuration retrieval for future reference and potential re-enablement.

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import * as vscode from "vscode";
 import { LinearTreeDataProvider } from "./views/linear";
-import { VercelTreeDataProvider } from "./views/vercel";
 
 export function activate(context: vscode.ExtensionContext) {
   // Get configuration
@@ -9,22 +8,22 @@ export function activate(context: vscode.ExtensionContext) {
   const linearApiKey = config.get<string>("linearApiKey");
   const linearTeam = config.get<string>("linearTeam");
 
-  const vercelToken = config.get<string>("vercelToken");
-  const vercelTeam = config.get<string>("vercelTeam");
+  // const vercelToken = config.get<string>("vercelToken");
+  // const vercelTeam = config.get<string>("vercelTeam");
 
   if (linearApiKey && linearTeam) {
     new LinearTreeDataProvider(context);
     // context.subscriptions.push(linearTreeDataProvider.dispose);
   }
 
-  if (vercelToken && vercelTeam) {
-    const vercelTreeDataProvider = new VercelTreeDataProvider(
-      vercelToken,
-      vercelTeam,
-      context.globalState,
-    );
-    context.subscriptions.push(vercelTreeDataProvider.dispose);
-  }
+  // if (vercelToken && vercelTeam) {
+  //   const vercelTreeDataProvider = new VercelTreeDataProvider(
+  //     vercelToken,
+  //     vercelTeam,
+  //     context.globalState,
+  //   );
+  //   context.subscriptions.push(vercelTreeDataProvider.dispose);
+  // }
 }
 
 export function deactivate() {}
